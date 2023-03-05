6ddad6b10ba1571f01c006f7dddcadbae4e799fa
naomi0427 <119838917+naomi0427@users.noreply.github.com>
2023-03-05 15:34:29 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+  validates :nickname,         presence: true
+  validates :last_name,        presence: true,  format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :first_name,       presence: true,  format: { with:/\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :last_name_kana,   presence: true,  format: { with: /\A[ァ-ヶー]+\z/, message: '全角文字を使用してください' }
+  validates :first_name_kana,  presence: true,  format: { with: /\A[ァ-ヶー]+\z/, message: '全角文字を使用してください' }
+  validates :birthday,         presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
