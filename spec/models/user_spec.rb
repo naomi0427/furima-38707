2276c05a4f549ad092bfe94ac119ea3f2dc3f50e
naomi0427 <119838917+naomi0427@users.noreply.github.com>
2023-03-08 12:49:15 +0900

    単体テストコード作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe "ユーザー新規登録" do
+    it "ユーザーの新規登録ができる" do
+      expect(@user).to be_valid
+    end
     it "メールアドレスが必須であること" do
+      #user = FactoryBot.build(:user)
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include ("Email can't be blank")
     end
     it "メールアドレスが一意性であること。" do
+      @user.save
+      another_user = FactoryBot.build(:user)
+      another_user.email = @user.email
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
     end
     it "メールアドレスは、@を含む必要があること" do
+      @user.email = 'sample'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
     end
     it "パスワードが必須であること" do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
     end
     it "パスワードは、6文字以上での入力が必須であること。" do
+      @user.password = 'abc'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
     end
     it "パスワードとパスワード（確認）は、値の一致が必須であること。" do
+      @user.password = 'sample0123'
+      @user.password_confirmation = 'sample456'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
   end
 end
