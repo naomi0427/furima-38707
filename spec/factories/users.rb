afc9bb8afa833c2b0436a50fd87a2ca280e95298
naomi0427 <119838917+naomi0427@users.noreply.github.com>
2023-03-08 12:48:44 +0900

    Factorybot導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
-    last_name             {'佐藤'}
-    first_name            {'健'}
-    last_name_kana        {'サトウ'}
-    first_name_kana       {'タケル'}
-    birthday              {'2000-01-01'}
+    last_name             {"佐藤"}
+    first_name            {"健"}
+    last_name_kana        {"サトウ"}
+    first_name_kana       {"タケル"}
+    birthday              {Faker::Date.birthday}
   end
 end
