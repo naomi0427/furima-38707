0384eecf4e4c6a064083b5eacb6607a565df9693
naomi0427 <119838917+naomi0427@users.noreply.github.com>
2023-03-12 15:31:04 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,19 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
   def index
   end
 
   def new
-    before_action :authenticate_user!
     @items = Item.new
   end
 
+  def create
+    Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:items_name, :explain, :category_id, :condition_id, :tax_id, :from_prefecture_id, :schedule_day_id, :price, :user)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,13 @@
 class Item < ApplicationRecord
+  validates :items_name,
+  validates :explain,
+  validates :category_id, 
+  validates :condition_id, 
+  validates :tax_id, 
+  validates :from_prefecture_id, 
+  validates :schedule_day_id,
+  validates :price,
 
-
-
-
-
-
-
-  validates : category_id, condition_id, tax_id, from_prefecture_id, schedule_day_id
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :tax, :from_prefecture, :schedule_day,
 end
