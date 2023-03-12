66694f7ac3721d596d07db4ed941d915a766fdb0
naomi0427 <119838917+naomi0427@users.noreply.github.com>
2023-03-12 12:32:01 +0900

    Newの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
 
+
+
+
+
+
+
+  validates : category_id, condition_id, tax_id, from_prefecture_id, schedule_day_id
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :tax, :from_prefecture, :schedule_day,
 end
