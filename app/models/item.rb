f621727ec8ce61ebb52704cb0b51fa69eabb1f82
naomi0427 <119838917+naomi0427@users.noreply.github.com>
2023-03-12 15:33:33 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,12 @@
 class Item < ApplicationRecord
-  validates :items_name,
-  validates :explain,
-  validates :category_id, 
-  validates :condition_id, 
-  validates :tax_id, 
-  validates :from_prefecture_id, 
-  validates :schedule_day_id,
-  validates :price,
+  validates :items_name,         presence: true
+  validates :explain,            presence: true
+  validates :category_id,        presence: true
+  validates :condition_id,       presence: true
+  validates :tax_id,             presence: true
+  validates :from_prefecture_id, presence: true
+  validates :schedule_day_id,    presence: true
+  validates :price,              presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :condition, :tax, :from_prefecture, :schedule_day,
