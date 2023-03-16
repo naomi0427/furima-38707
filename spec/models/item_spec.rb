1281699832c81848cf58a6103eaf02443a219424
naomi0427 <119838917+naomi0427@users.noreply.github.com>
2023-03-16 12:41:31 +0900

    出品できる時のテストコードの作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe "商品出品" do
+    context "出品できる時" do
+      it "項目が全て条件をクリアした時登録ができる" do
+       expect(@item).to be_valid
+      end
+    end
+    context "出品できない時" do
+  end
 end
