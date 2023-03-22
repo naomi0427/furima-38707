27220a90398904c98e89cb21966b43e5ce391838
naomi0427 <119838917+naomi0427@users.noreply.github.com>
2023-03-22 20:05:10 +0900

    updateルートとコントローラー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,17 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
   end
-  
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+    redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
   def new
     @item = Item.new
   end
