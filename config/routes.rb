6c0a5897dd10b8665c305515796adce867656556
naomi0427 <119838917+naomi0427@users.noreply.github.com>
2023-04-01 03:09:07 +0900

    orderルート作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to:"items#index"
-  resources :items, only: [:new, :create, :index, :show, :edit, :update, :destroy ]
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end
 end
