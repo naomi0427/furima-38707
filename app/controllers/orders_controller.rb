3779a4a07a90c8a1bf5866219ccc3fded5e713d3
naomi0427 <119838917+naomi0427@users.noreply.github.com>
2023-04-01 22:00:42 +0900

    orderコントローラー作成2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,16 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new()
+    @order = Order.new(order_params)
+  end
+
+
+
+
+
+  private
+
+  def order_params
+    params.require(:order).permit(:post_code, :from_prefecture_id, :to_city, :to_house_number, :to_building_name, :to_telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
