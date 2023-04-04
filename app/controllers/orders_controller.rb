c3ca3380648f208e060a50f56d76f28aab2e51b4
naomi0427 <119838917+naomi0427@users.noreply.github.com>
2023-04-04 23:04:08 +0900

    Formオブジェクトの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,35 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create]
+
   def index
-    #@order = Order.new
+    @order = Order.new
+  end
+
+  def new
+    @order = Order.new
   end
 
   def create
-    binding.pry
-    #@order = Order.new(order_params)
+    @order = Order.new(after_sale_params)
+    if @order.valid?
+      @order.save
+      redirect_to root_path
+    else
+     render 'index'
+    end
   end
-
-
-
-
 
   private
 
-  def order_params
-    params.require(:order).permit(:post_code, :from_prefecture_id, :to_city, :to_house_number, :to_building_name, :to_telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  def after_sale_params
+    params.require(:order).permit(:post_code, :from_prefecture_id, :to_city, :to_house_number, :to_building_name, :to_telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+
+  #if current_user.id == @item.user_id or @item.after_sale.present?
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,16 +1,21 @@
-class Order < ApplicationRecord
+class Order 
   include ActiveModel::Model
-  attr_accessor :image, :post_code, :from_prefecture_id, :to_city, :to_house_number, :to_building_name, :to_telephone_number, :item_id, :user_id, :token
-   # ここにバリデーションの処理を書く
-  validates :post_code,                presence: true
-  validates :from_prefecture_id,       presence: true
-  validates :to_city,                  presence: true
-  validates :to_house_number,          presence: true
-  validates :to_telephone_number,      presence: true
-  validates :item_id,                  presence: true
-  validates :user_id,                  presence: true
-  #validates :token                     presence: true
-   def save
+  attr_accessor :post_code, :from_prefecture_id, :to_city, :to_house_number, :to_building_name, :to_telephone_number, :item_id, :user_id, :token
+  with_options presence: true do
+    validates :post_code     
+    validates :from_prefecture_id
+    validates :to_city
+    validates :to_house_number
+    validates :to_telephone_number
+    validates :item_id
+    validates :user_id
+    #validates :token
+
+  end
+
+  validates :from_prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+
+  def save
     # 各テーブルにデータを保存する処理を書く
     after_sale = AfterSale.create(item_id: item_id, user_id: user_id)
 
