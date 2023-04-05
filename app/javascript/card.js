e5acf61b87a596b01ff62f06c9a4bd597690a9ab
naomi0427 <119838917+naomi0427@users.noreply.github.com>
2023-04-06 00:31:01 +0900

    ビューの編集

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,15 @@
 const pay = () => {
   const payjp = Payjp('pk_test_499074e0afdb2073c10c23a8')// PAY.JPテスト公開鍵
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#card-number');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
+
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
