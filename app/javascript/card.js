ab529335f551f4fa072bda60576a1d3fa639d674
naomi0427 <119838917+naomi0427@users.noreply.github.com>
2023-04-06 23:56:45 +0900

    環境変数の呼び込み

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_499074e0afdb2073c10c23a8')// PAY.JPテスト公開鍵
+  const payjp = Payjp(process.env.PAYJP_PUBLIC_KEY);
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
